5a9e9f1bcca89ae983e570a22dbe482f37c5cde1
Jonas <jonaso@web.de>
2017-07-23 12:36:13 +0200

    setup

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ DEV
 > vagrant up
 
 To monitor:
+
 > vagrant ssh
+>
 > tail -f /var/log/deepstream/deepstream.2017-07-23
 
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+  // context: path.join(__dirname, "src"),
+  entry: {
+    // vendor: [
+    //   "react",
+    //   "react-dom",
+    //   "react-tap-event-plugin",
+    //   "whatwg-fetch", // AJAX fetch polyfill - https://github.com/github/fetch
+    //   "material-ui/styles/MuiThemeProvider"
+    // ],
+    bundle: "./src/index.js"
+  },
+  output: {
+    path: path.resolve('./dist'),
+    publicPath: '/',
+    filename: "[name].js"
+  },
+  devtool: debug ? "inline-sourcemap" : null,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|vendor|venv)/,
+        loader: 'babel', //-loader (optional)
+        query: {
+          // cacheDirectory: true,
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-decorators-legacy', 'react-html-attrs', 'transform-class-properties'],
+        }
+      },
+      // fonts and svg
+      // { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      // { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      // { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      // { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      // images
+      {
+        test: /\.(ico|jpe?g|png|gif)$/,
+        loader: "file"
+      },
+      {
+        test: /\.scss$/,
+        exclude: [ /vendor/, /node_modules/, /venv/ ],
+        loader: ExtractTextPlugin.extract("style", "css?sourceMap!postcss!sass?sourceMap&outputStyle=expanded")
+      },
+      {
+        test: /\.css$/,
+        loader: [ 'style-loader', 'css-loader' ].join("!")
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.jsx', '.js', '.scss', '.css'],
+    modulesDirectories: [
+      'node_modules',
+      'src/Components'
+    ]
+  },
+  plugins: debug ?
+  //DEV
+  [
+    new ExtractTextPlugin('style.css', {
+      // allChunks: true
+    }),
+    //new CopyWebpackPlugin([
+    //  {from: './data', to: './data'}
+    //]),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+  :
+  [
+    new ExtractTextPlugin('style.css', {
+      // allChunks: true
+    }),
+    //new CopyWebpackPlugin([
+    //  {from: './data', to: './data'}
+    //]),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+  	new webpack.optimize.UglifyJsPlugin({
+        // mangle: false,
+        compress: {
+            warnings: false
+        }
+     })
+  ]
+};
