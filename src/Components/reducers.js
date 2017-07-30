c794bd136a12b8d0f8a6789c289e2fbe11348d78
Jonas <jonaso@web.de>
2017-07-30 21:22:52 +0200

    redux: actions; reducers; store

--- src/Components/reducers.js
+++ src/Components/reducers.js
@@ -3,33 +3,38 @@
  */
 
 import { combineReducers } from 'redux'
+import { initialState } from './store'
 
-import { ADD_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+import {
+    FOLLOW_USER,
+    REPLY_THREAD,
+    COMMENT_PROFILE,
+    TOGGLE_SITEMENU,
+    SET_VISIBILITY_FILTER,
+    VisibilityFilters
+} from './actions'
 
-// const { SHOW_ALL } = VisibilityFilters
-
-const initialState = {
-    visibilityFilter: VisibilityFilters.SHOW_ALL,
-    feedItems: []
-}
 
 function feedFilter(state = initialState, action) {
+    if (typeof state === 'undefined') {
+        return initialState
+    }
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
             return Object.assign({}, state, {
               visibilityFilter: action.filter
             });
-		case FILTER_ITEMS:
-		  return Object.assign({}, state, {
-		    feedItems: state.feedItems.map((project, index) => {
-		      if (index === action.index) {
-		        return Object.assign({}, feedItem, {
-		          completed: !feedItem.completed // TODO
-		        });
-		      }
-		      return feedItem;
-		    })
-		  })
+        case FILTER_ITEMS:
+          return Object.assign({}, state, {
+            feedItems: state.feedItems.map((project, index) => {
+              if (index === action.index) {
+                return Object.assign({}, feedItem, {
+                  completed: !feedItem.completed
+                });
+              }
+              return feedItem;
+            })
+          })
         default:
             return state;
     }
