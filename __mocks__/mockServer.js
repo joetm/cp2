de98f14821295f9b79a2421c9f59606c24b8b12b
Jonas <jonaso@web.de>
2017-08-12 14:56:43 +0200

    mock server

---
+++ __mocks__/mockServer.js
@@ -0,0 +1,23 @@
+
+/**
+ * A mock REST server for DEV
+ **/
+
+import mockState from './mockState'
+
+
+const delay = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+export const fetchData(key) => {
+    delay(500)
+        .then(() => {
+            try {
+                return mockState[key]
+            } catch (e) {
+                throw new Error(e)
+            }
+        })
+}
