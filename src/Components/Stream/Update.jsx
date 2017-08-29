55db1994033bd929709d29d0173b1195ab8aa94d
Jonas <jonaso@web.de>
2017-08-29 21:02:12 +0200

    max height of image in Update

--- src/Components/Stream/Update.jsx
+++ src/Components/Stream/Update.jsx
@@ -9,6 +9,46 @@ import { LikeButton, FavoriteButton, CommentButton } from './Button'
 import { navigateTo } from '../../common/helpers'
 
 
+// https://stackoverflow.com/a/39094233/426266
+class AtomicImage extends React.PureComponent {
+   constructor(props) {
+        super(props)
+        this.state = {dimensions: {}}
+    }
+    onImgLoad = ({target: img}) => {
+        this.setState({
+          dimensions: {
+            height: img.offsetHeight,
+            width: img.offsetWidth,
+          }
+        })
+    }
+    render() {
+        const {src} = this.props
+        const { width, height } = this.state.dimensions
+        // the images should always be scaled to the max dimension of the browser window
+        // console.log(window.innerWidth)
+        // console.log(window.innerHeight)
+        const dynamicStyle = {}
+        // case: portrait image
+//        if (width <= height) {
+          dynamicStyle.maxHeight = `${window.innerHeight}px`
+          dynamicStyle.width = 'auto'
+//        } else {
+//          dynamicStyle.maxWidth = `${window.innerWidth}px`
+//          dynamicStyle.height = 'auto'
+//        }
+        return (
+            <img
+              style={ dynamicStyle }
+              onLoad={this.onImgLoad}
+              alt=""
+              src={src}
+            />
+        )
+    }
+}
+
 
 const Update = (props) => {
 
@@ -22,14 +62,14 @@ const Update = (props) => {
   const showTitle = props.showTitle === false ? false : true
 
   let url = '/'
-  let img
+  let imgSrc
   switch(type) {
     case 'image':
       url = `/images/${id}`
-      img = src
+      imgSrc = src
     case 'video':
       url = `/videos/${id}`
-      img = thumb
+      imgSrc = thumb
   }
 
   return (
@@ -41,7 +81,13 @@ const Update = (props) => {
         style={{cursor: 'pointer'}}
       >
         <CardMedia>
-          <img src={img} alt="" />
+          {/*
+          <AtomicImage src={imgSrc} />
+          */}
+          <img
+            src={imgSrc}
+            alt=""
+          />
         </CardMedia>
         <CardHeader
           title={username}
